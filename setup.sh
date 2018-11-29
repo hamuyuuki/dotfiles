5927febef6f2f85cdf36998377cb15e1fa6ac974
hamuyuuki32 <hamuyuuki32@gmail.com>
2018-11-30 07:48:08 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ setup() {
     }
 
     package_install() {
-      if [ `uname` = "Darwin" ];then
-        brew install $1
-      else
-        sudo yum -y install $1
-      fi
+        if [ `uname` = "Darwin" ];then
+            brew install $1
+        else
+            sudo yum -y install $1
+        fi
     }
 
     init_git() {
@@ -37,28 +37,28 @@ setup() {
         package_install zsh
         sudo chsh -s /bin/zsh $USER
         if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
-          git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+            git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-          symlink "$HOME/.zprezto/runcoms/zlogin" "$HOME/.zlogin"
-          symlink "$HOME/.zprezto/runcoms/zshrc" "$HOME/.zshrc"
-          symlink "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
-          symlink "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
-          symlink "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
-          symlink "$HOME/.zprezto/runcoms/zlogout" "$HOME/.zlogout"
+            symlink "$HOME/.zprezto/runcoms/zlogin" "$HOME/.zlogin"
+            symlink "$HOME/.zprezto/runcoms/zshrc" "$HOME/.zshrc"
+            symlink "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
+            symlink "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
+            symlink "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
+            symlink "$HOME/.zprezto/runcoms/zlogout" "$HOME/.zlogout"
 
-          zsh -c 'setopt EXTENDED_GLOB'
-          # zsh -c 'prompt steeef'
+            zsh -c 'setopt EXTENDED_GLOB'
+            # zsh -c 'prompt steeef'
 
-          echo "alias ll='ls -al'" >> "$HOME/.zshrc"
-          echo "alias t='tmux attach -t tmux || tmux new-session -s tmux \; source-file ~/.tmux.session'" >> "$HOME/.zshrc"
-          echo "autoload -U compinit" >> "$HOME/.zshrc"
-          echo "compinit" >> "$HOME/.zshrc"
-          echo "autoload history-search-end" >> "$HOME/.zshrc"
-          echo "zle -N history-beginning-search-backward-end history-search-end" >> "$HOME/.zshrc"
-          echo "zle -N history-beginning-search-forward-end history-search-end" >> "$HOME/.zshrc"
-          echo "bindkey '^[OA' history-beginning-search-backward-end" >> "$HOME/.zshrc"
-          echo "bindkey '^[OB' history-beginning-search-forward-end" >> "$HOME/.zshrc"
-          echo "bindkey '^P' history-beginning-search-backward-end" >> "$HOME/.zshrc"
+            echo "alias ll='ls -al'" >> "$HOME/.zshrc"
+            echo "alias t='tmux attach -t tmux || tmux new-session -s tmux \; source-file ~/.tmux.session'" >> "$HOME/.zshrc"
+            echo "autoload -U compinit" >> "$HOME/.zshrc"
+            echo "compinit" >> "$HOME/.zshrc"
+            echo "autoload history-search-end" >> "$HOME/.zshrc"
+            echo "zle -N history-beginning-search-backward-end history-search-end" >> "$HOME/.zshrc"
+            echo "zle -N history-beginning-search-forward-end history-search-end" >> "$HOME/.zshrc"
+            echo "bindkey '^[OA' history-beginning-search-backward-end" >> "$HOME/.zshrc"
+            echo "bindkey '^[OB' history-beginning-search-forward-end" >> "$HOME/.zshrc"
+            echo "bindkey '^P' history-beginning-search-backward-end" >> "$HOME/.zshrc"
         fi
     }
 
@@ -94,25 +94,38 @@ setup() {
     }
 
     init_rbenv() {
-      git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-      git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-      echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> "$HOME/.zshrc"
-      echo 'eval "$(rbenv init -)"' >> "$HOME/.zshrc"
-      exec $SHELL -l
+        git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+        git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> "$HOME/.zshrc"
+        echo 'eval "$(rbenv init -)"' >> "$HOME/.zshrc"
+        exec $SHELL -l
     }
 
     init_golang() {
-      package_install golang
-      echo 'export GOPATH=$HOME' >> "$HOME/.zshrc"
-      echo 'export PATH=$PATH:$GOPATH/bin' >> "$HOME/.zshrc"
-      exec $SHELL -l
+        package_install golang
+        echo 'export GOPATH=$HOME' >> "$HOME/.zshrc"
+        echo 'export PATH=$PATH:$GOPATH/bin' >> "$HOME/.zshrc"
+        exec $SHELL -l
     }
 
     init_ghq() {
-      go get github.com/motemen/ghq
-      echo 'function fzf-src () { cd $(ghq list -p | fzf)  }' >> "$HOME/.zshrc"
-      echo 'zle -N fzf-src' >> "$HOME/.zshrc"
-      echo 'bindkey "^]" fzf-src' >> "$HOME/.zshrc"
+        go get github.com/motemen/ghq
+        echo 'function fzf-src () { cd $(ghq list -p | fzf)  }' >> "$HOME/.zshrc"
+        echo 'zle -N fzf-src' >> "$HOME/.zshrc"
+        echo 'bindkey "^]" fzf-src' >> "$HOME/.zshrc"
+    }
+
+    init_virtualbox() {
+        brew cask install virtualbox
+    }
+
+    init_vagrant() {
+        brew cask install vagrant
+        ghq get https://github.com/coreos/coreos-vagrant.git
+    }
+
+    init_node() {
+        brew install nodebrew
     }
 
     init_git
@@ -125,6 +138,9 @@ setup() {
     init_rbenv
     init_golang
     init_ghq
+    init_virtualbox
+    init_vagrant
+    init_node
     init_vim
 }
 
