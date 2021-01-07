96c45dfd2e0476178a47d069fe52192ad8f13ed9
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-01-07 10:32:24 +0900

    Update the setup script with macOS Catalina 10.15

--- host_machine/setup.sh
+++ host_machine/setup.sh
@@ -5,8 +5,19 @@ set -e
 set -u
 set -v
 
+# Clone dotfiles repository
+git clone https://github.com/hamuyuuki/dotfiles ~/.dotfiles
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew packages
-brew bundle
+brew bundle --file=~/.dotfiles/host_machine/Brewfile
+
+# Setup Karabiner-Elements
+mkdir -p ~/.config/karabiner
+ln -s ~/.dotfiles/host_machine/karabiner.json ~/.config/karabiner/karabiner.json
+
+# Setup Vagrant
+mkdir -p ~/vagrant/ubuntu
+ln -s ~/.dotfiles/host_machine/Vagrantfile ~/vagrant/ubuntu/Vagrantfile
